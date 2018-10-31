2dbb7e9ae40bd1bcd42540287d1c6744aaf7ab0b
stayseek <stayseek0@gmail.com>
2018-10-31 21:54:12 +0300

    На выдачу.

---
+++ Lesson_8/WFStudents/AboutForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WFStudents
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbDateTime = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.nudBinded = new System.Windows.Forms.NumericUpDown();
+            this.tbBinded = new System.Windows.Forms.TextBox();
+            this.lblTile = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblAuthor = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBinded)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(34, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(215, 215);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbDateTime);
+            this.groupBox1.Location = new System.Drawing.Point(282, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(152, 229);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Свойства (properties) структуры DateTime";
+            // 
+            // tbDateTime
+            // 
+            this.tbDateTime.AcceptsReturn = true;
+            this.tbDateTime.Location = new System.Drawing.Point(6, 31);
+            this.tbDateTime.Multiline = true;
+            this.tbDateTime.Name = "tbDateTime";
+            this.tbDateTime.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbDateTime.Size = new System.Drawing.Size(141, 192);
+            this.tbDateTime.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.nudBinded);
+            this.groupBox2.Controls.Add(this.tbBinded);
+            this.groupBox2.Location = new System.Drawing.Point(282, 247);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(152, 75);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Связка элементов";
+            // 
+            // nudBinded
+            // 
+            this.nudBinded.Location = new System.Drawing.Point(6, 19);
+            this.nudBinded.Name = "nudBinded";
+            this.nudBinded.Size = new System.Drawing.Size(140, 20);
+            this.nudBinded.TabIndex = 1;
+            // 
+            // tbBinded
+            // 
+            this.tbBinded.Enabled = false;
+            this.tbBinded.Location = new System.Drawing.Point(6, 45);
+            this.tbBinded.Name = "tbBinded";
+            this.tbBinded.Size = new System.Drawing.Size(140, 20);
+            this.tbBinded.TabIndex = 0;
+            // 
+            // lblTile
+            // 
+            this.lblTile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTile.Location = new System.Drawing.Point(24, 238);
+            this.lblTile.Name = "lblTile";
+            this.lblTile.Size = new System.Drawing.Size(236, 26);
+            this.lblTile.TabIndex = 3;
+            this.lblTile.Text = "Программа учёта студентов";
+            this.lblTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.Location = new System.Drawing.Point(34, 272);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(215, 14);
+            this.lblVersion.TabIndex = 4;
+            this.lblVersion.Text = "v 1.0";
+            this.lblVersion.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblAuthor
+            // 
+            this.lblAuthor.Location = new System.Drawing.Point(31, 292);
+            this.lblAuthor.Name = "lblAuthor";
+            this.lblAuthor.Size = new System.Drawing.Size(218, 23);
+            this.lblAuthor.TabIndex = 5;
+            this.lblAuthor.Text = "Автор: Станислав Митрофанов";
+            this.lblAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 332);
+            this.Controls.Add(this.lblAuthor);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.lblTile);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AboutForm";
+            this.Text = "О программе";
+            this.Load += new System.EventHandler(this.AboutForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBinded)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbDateTime;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown nudBinded;
+        private System.Windows.Forms.TextBox tbBinded;
+        private System.Windows.Forms.Label lblTile;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblAuthor;
+    }
+}
